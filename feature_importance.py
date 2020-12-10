dcb0f72a7b2029dc885635d26f7254e083b5c8ea
Yuxuan <yuxuan.wu17@gmail.com>
2020-12-10 22:58:11 +0800

    feature_importance.py plot could be successfully plotted

---
+++ feature_importance.py
@@ -0,0 +1,35 @@
+from sklearn import svm
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+def feature_plot(classifier, feature_names, top_features=6):
+    coef = classifier.coef_.ravel()
+    top_positive_coefficients = np.argsort(coef)[-top_features:]
+    top_negative_coefficients = np.argsort(coef)[:top_features]
+    top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
+    plt.figure(figsize=(18, 7))
+    colors = ['green' if c < 0 else 'blue' for c in coef[top_coefficients]]
+    plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
+    feature_names = np.array(feature_names)
+    plt.xticks(np.arange(2 * top_features), feature_names[top_coefficients], rotation=45, ha='right')
+
+    plt.show()
+
+
+heart_data = pd.read_csv("/home/yuxuan/kaggle/heart_failure_clinical_records_dataset.csv")
+
+accuracy_list = []
+
+X = heart_data.iloc[:, 0:11]
+X = StandardScaler().fit_transform(X)
+y = heart_data['DEATH_EVENT']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, random_state=1)
+
+print(heart_data.drop(['DEATH_EVENT', 'time'], axis=1).columns.values)
+
+trainedsvm = svm.LinearSVC().fit(X, y)
+feature_plot(trainedsvm, heart_data.drop(['DEATH_EVENT', 'time'], axis=1).columns.values)
